08a801c04497cde4e6eae95fe16ccd272f92d3c7
PANZ5 <panz5@student.op.ac.nz>
2022-10-26 16:03:00 +0800

    Done player control & animation

--- Pan_Moblie game dev_farming/Assets/Scripts/Player/PlayerController.cs
+++ Pan_Moblie game dev_farming/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    // Animators
+    public Animator bodyAnimator;
+    public Animator armAnimator;
+    public Animator hairAnimator;
+
     Vector2 movement;
 
     // Update is called once per frame
@@ -15,6 +20,20 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
+
+        // Animations
+        // Body
+        bodyAnimator.SetFloat("Horizontal", movement.x);
+        bodyAnimator.SetFloat("Vertical", movement.y);
+        bodyAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        // Arm
+        armAnimator.SetFloat("Horizontal", movement.x);
+        armAnimator.SetFloat("Vertical", movement.y);
+        armAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        // Hair
+        hairAnimator.SetFloat("Horizontal", movement.x);
+        hairAnimator.SetFloat("Vertical", movement.y);
+        hairAnimator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     // FixedUpdate is called once per physics update (50 times per second)
